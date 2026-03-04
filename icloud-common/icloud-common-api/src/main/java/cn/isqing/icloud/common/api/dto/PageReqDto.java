c9342b746530c1b0a3504803219f4b350c467b5c
songqingwei <songqingwei@aliyun.com>
2026-03-04 11:36:14 +0800

    feat(common): 添加PageReqDto分页信息获取方法
    - 新增getCurrent方法返回当前页码
    - 新增getPageSize方法返回每页大小
    - 新增isNeedTotal方法判断是否需要总数
    - 新增getOffset方法计算偏移量
    - 优化getOffset方法的代码格式和可读性

--- icloud-common/icloud-common-api/src/main/java/cn/isqing/icloud/common/api/dto/PageReqDto.java
+++ icloud-common/icloud-common-api/src/main/java/cn/isqing/icloud/common/api/dto/PageReqDto.java
@@ -3,6 +3,7 @@ package cn.isqing.icloud.common.api.dto;
 import lombok.Data;
 
 import jakarta.validation.Valid;
+
 import java.io.Serializable;
 
 /**
@@ -27,8 +28,25 @@ public class PageReqDto<T> implements Serializable {
 
         private Long fromId;
 
-        public Long getOffset(){
-            return (long)(pageNum-1)*pageSize;
+        public Long getOffset() {
+            return (long) (pageNum - 1) * pageSize;
         }
+    }
+
+
+    public Integer getCurrent() {
+        return pageInfo != null ? pageInfo.getPageNum() : 1;
+    }
+
+    public Integer getPageSize() {
+        return pageInfo != null ? pageInfo.getPageSize() : 10;
+    }
+
+    public Boolean isNeedTotal() {
+        return pageInfo != null ? pageInfo.needTotal : true;
+    }
+
+    public Long getOffset() {
+        return pageInfo != null ? pageInfo.getOffset() : 0L;
     }
 }
